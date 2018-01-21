7a759279a62b928570a5716f58e5a50edc8c652e
CA\jamesxlee <jamesxlee@deloitte.ca>
2018-01-21 13:56:24 -0500

    1. Create the action which gets called.

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,9 +3,20 @@ import axios from "axios";
 // Use a constant for action types, so that the compiler may catch typos.
 export const FETCH_LISTS = "fetch_lists";
 export const ADD_ITEM = 'add_item';
+export const UPDATE_SCORE = 'update_score';
 
 // saves the root url as a variable
 const rootUrl = `http://lists.hackeryou.com`;
+
+export function updateScore(itemId, currentScore, value) {
+  let score = currentScore + value;
+  const request = axios.post(`${rootUrl}/item/${itemId}`, {score});
+
+  return {
+    type: UPDATE_SCORE,
+    payload: request
+  };
+}
 
 export function addItem(listId, item) {
   const request = axios.post(`${rootUrl}/list/${listId}/item`, {item});

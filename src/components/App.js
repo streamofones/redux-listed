46dd03f98b1c052acb406fdc3a67d694bb017866
CA\jamesxlee <jamesxlee@deloitte.ca>
2018-01-21 11:11:48 -0500

    Update App with styling and content

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import '../index.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hellooooooo Redux</h1>
+        <header>
+          <h1>Listed</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="add a list"
+            />
+          </form>
+        </header>
+        <main>Lists go here</main>
       </div>
     );
   }

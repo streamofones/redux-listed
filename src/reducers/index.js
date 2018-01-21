576825c6402dd9f0ba0e671d686665db55ba266a
CA\jamesxlee <jamesxlee@deloitte.ca>
2018-01-21 11:07:18 -0500

    Hook up the list reducer to the main reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import ListReducer from './reducer_lists';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  lists: ListReducer
 });
 
 

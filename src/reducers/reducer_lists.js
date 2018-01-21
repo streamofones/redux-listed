91ef61e9506d8c8b7f313108bc2c5dc8906cd932
CA\jamesxlee <jamesxlee@deloitte.ca>
2018-01-21 11:47:13 -0500

    Based on the fetch lists action, retrieve the payload

--- src/reducers/reducer_lists.js
+++ src/reducers/reducer_lists.js
@@ -1,3 +1,14 @@
+import _ from 'underscore';
+
+import {FETCH_LISTS} from "../actions"
+
 export default function(state = {}, action) {
-  return state;
+  switch (action.type) {
+    case FETCH_LISTS:
+      // this underscores method takes a datalist and a desired key for
+      // each item and returns an object with an index of each item.
+      return _.indexBy(action.payload.data, "_id");
+    default:
+      return state;
+  }
 }

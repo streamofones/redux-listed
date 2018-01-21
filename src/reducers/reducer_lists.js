0fda7b9a65085962303de5c986493432a242491c
CA\jamesxlee <jamesxlee@deloitte.ca>
2018-01-21 13:36:52 -0500

    Dynamically update the list with the latest item once it has been submitted

--- src/reducers/reducer_lists.js
+++ src/reducers/reducer_lists.js
@@ -1,9 +1,23 @@
 import _ from 'underscore';
 
-import {FETCH_LISTS} from "../actions"
+import {FETCH_LISTS} from "../actions";
+import {ADD_ITEM} from "../actions";
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case ADD_ITEM: {
+      const newItem = action.payload.data.item;
+      const listId = action.payload.data.item.belongs_to;
+
+      // ... is to spread
+      return {
+        ...state,
+        [listId]: {
+          ...state[listId],
+          items: [...state[listId].items, newItem]
+        }
+      };
+    }
     case FETCH_LISTS:
       // this underscores method takes a datalist and a desired key for
       // each item and returns an object with an index of each item.

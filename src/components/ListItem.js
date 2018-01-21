5e8daeb42b87c06e26ed8a716aa8c67d77817d45
CA\jamesxlee <jamesxlee@deloitte.ca>
2018-01-21 14:03:16 -0500

    4. Add action to the deconstruction, call the action

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,17 +4,17 @@ import React from "react";
 import upvote from '../images/upvote.png';
 import downvote from '../images/downvote.png';
 
-const ListItem = ({ itemInfo }) => {
+const ListItem = ({ itemInfo, updateScore }) => {
   return (
     <li className="list__item">
       {itemInfo.item}
       <div className="item__scoreCard">
         <span className="scoreCard__score">{itemInfo.score}</span>
-        <button>
+        <button onClick={() => updateScore(itemInfo._id, itemInfo.score, 1)}>
           <img src={upvote} alt="thumbs up" />
           Upvote
         </button>
-        <button>
+        <button onClick={() => updateScore(itemInfo._id, itemInfo.score, -1)}>
           <img src={downvote} alt="thumbs down" />Downvote
         </button>
       </div>

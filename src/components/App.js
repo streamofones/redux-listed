1b2326e146049129a0b9aace3780370b76e48be7
CA\jamesxlee <jamesxlee@deloitte.ca>
2018-01-21 14:12:09 -0500

    2. Add the action into the App

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { fetchLists } from '../actions/index';
+import { fetchLists, addList } from '../actions/index';
 import List from './List';
 import '../index.css';
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      newList: ''
+    };
     this.renderLists = this.renderLists.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount() {
     this.props.fetchLists()
       .then(res => console.log(res));
+  }
+  handleChange(e) {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+  handleSubmit(e) {
+    e.preventDefault(); // Stop the page from submitting automatically.
+
+    let newList = this.state.newList.trim();
+
+    if (newList !== '') {
+      this.props.addList(newList);
+    }
+
+    this.setState({
+      newList: "" // Reset the newItem after submission.
+    });
   }
   renderLists(){
     const lists = this.props.lists;
@@ -27,8 +50,10 @@ class App extends Component {
       <div className="App">
         <header>
           <h1>Listed</h1>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <input
+              onChange={this.handleChange}
+              value={this.state.newList}
               type="text"
               placeholder="add a list"
             />

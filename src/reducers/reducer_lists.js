57ed1327696c854ad6d00447f18ed6ad8673a801
CA\jamesxlee <jamesxlee@deloitte.ca>
2018-01-21 14:04:06 -0500

    Update Reducer to update state from redux store

--- src/reducers/reducer_lists.js
+++ src/reducers/reducer_lists.js
@@ -1,7 +1,6 @@
 import _ from 'underscore';
 
-import {FETCH_LISTS} from "../actions";
-import {ADD_ITEM} from "../actions";
+import {FETCH_LISTS, ADD_ITEM, UPDATE_SCORE} from "../actions";
 
 export default function(state = {}, action) {
   switch (action.type) {
@@ -17,6 +16,25 @@ export default function(state = {}, action) {
           items: [...state[listId].items, newItem]
         }
       };
+    }
+    case UPDATE_SCORE: {
+      const newState = JSON.parse(JSON.stringify(state));
+
+      // create some variables from our ajx response.
+      const listId = action.payload.data.item.belongs_to;
+      const newItem = action.payload.data.item;
+      const itemId = action.payload.data.item._id;
+
+      // grab the array of list items
+      let listItems = newState[listId].items;
+
+      // search for the index of the list item we are updated
+      const index = listItems.findIndex(item => item._id == itemId);
+
+      // mutate the NEW COPY of state itself
+      listItems[index] = newItem;
+
+      return newState;
     }
     case FETCH_LISTS:
       // this underscores method takes a datalist and a desired key for
